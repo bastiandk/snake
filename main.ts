bec91e409ebdbad34d5687d88dcfe35ac57527f8
bastiandk <127124913+bastiandk@users.noreply.github.com>
2023-03-07 16:22:52 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,43 +11,102 @@ input.onButtonPressed(Button.B, function () {
     }
 })
 let runt = 0
+let snake_3x = 1e+21
+let snake_3y = 1e+21
+let snake_2x = -100000000000000000000
+let snake_2y = -100000000000000000000
+let tæller = 0
 let eat1_x = randint(0, 4)
 let eat1_y = randint(0, 4)
 runt = 0
 let snake_1x = 2
-let snake_1y = 2
+let snake_1y = 3
 led.plot(snake_1x, snake_1y)
 led.plot(eat1_x, eat1_y)
-basic.showString("Hello, Bastian!")
 loops.everyInterval(1000, function () {
+    if (tæller >= 2) {
+        led.unplot(snake_3x, snake_3y)
+        snake_3x = snake_2x
+        snake_3y = snake_2y
+    }
+    if (tæller >= 1) {
+        led.unplot(snake_2x, snake_2y)
+        snake_2x = snake_1x
+        snake_2y = snake_1y
+    }
     if (runt == 1) {
         led.unplot(snake_1x, snake_1y)
         snake_1x = snake_1x - 1
-        led.plot(snake_1x, snake_1y)
     } else if (runt == 0) {
         led.unplot(snake_1x, snake_1y)
         snake_1y = snake_1y - 1
-        led.plot(snake_1x, snake_1y)
     } else if (runt == -1) {
         led.unplot(snake_1x, snake_1y)
         snake_1x = snake_1x + 1
-        led.plot(snake_1x, snake_1y)
     } else {
         led.unplot(snake_1x, snake_1y)
         snake_1y = snake_1y + 1
-        led.plot(snake_1x, snake_1y)
     }
+    led.plot(snake_1x, snake_1y)
+    led.plot(snake_2x, snake_2y)
+    led.plot(snake_3x, snake_3y)
 })
 basic.forever(function () {
-    if (eat1_x == snake_1x && eat1_y == snake_1y) {
+    if (snake_1x > 4) {
+        basic.showString("u")
+    }
+    if (eat1_x < 0) {
+        basic.showString("u")
+    }
+    if (eat1_y > 4) {
+        basic.showString("u")
+    }
+    if (eat1_y < 0) {
+        basic.showString("u")
+    }
+    if (eat1_x == snake_1x && (eat1_y == snake_1y && tæller == 0)) {
         if (runt == 1) {
-        	
+            music.playMelody("- - C5 B - - - - ", 120)
+            snake_2x = snake_1x + 1
+            snake_2y = snake_1y
         } else if (runt == 0) {
-        	
+            music.playMelody("B - - - A - - - ", 120)
+            snake_2y = snake_1y - 1
+            snake_2x = snake_1x
         } else if (runt == -1) {
-        	
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            snake_2x = snake_1x - 1
+            snake_2y = snake_1y
         } else {
-        	
+            music.playMelody("- - - - D - - B ", 120)
+            snake_2y = snake_1y - 1
+            snake_2x = snake_1x
         }
+        led.plot(snake_2x, snake_2y)
+        tæller = 1
+        eat1_x = randint(0, 4)
+        eat1_y = randint(0, 4)
+        led.plot(eat1_x, eat1_y)
+    }
+    if (eat1_x == snake_1x && (eat1_y == snake_1y && tæller == 1)) {
+        if (runt == 1) {
+            music.playMelody("- - C5 B - - - - ", 120)
+            snake_3x = snake_2x + 1
+            snake_3y = snake_2y
+        } else if (runt == 0) {
+            music.playMelody("B - - - A - - - ", 120)
+            snake_3x = snake_2x - 1
+            snake_3y = snake_2y
+        } else if (runt == -1) {
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            snake_3x = snake_2x - 1
+            snake_3y = snake_2y
+        } else {
+            music.playMelody("- - - - D - - B ", 120)
+            snake_3x = snake_2x - 1
+            snake_3y = snake_2y
+        }
+        led.plot(snake_3x, snake_3y)
+        tæller = 2
     }
 })
